a1e253c0a9be147038ad448c33d076f0751cd67f
engrjeff <engineerjeff15@gmail.com>
2025-05-19 09:06:16 +0800

    change prisma generator

--- prisma/migrations/20250517105928_add_is_organizer_field_to_trip_member/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `isOrganizer` to the `TripMember` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TripMember" ADD COLUMN     "isOrganizer" BOOLEAN NOT NULL;
--- prisma/migrations/20250517110633_add_cascade_delete_on_tripcircle/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "TripMember" DROP CONSTRAINT "TripMember_tripCircleId_fkey";
-
--- AddForeignKey
-ALTER TABLE "TripMember" ADD CONSTRAINT "TripMember_tripCircleId_fkey" FOREIGN KEY ("tripCircleId") REFERENCES "TripCircle"("id") ON DELETE CASCADE ON UPDATE CASCADE;
